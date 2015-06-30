dc458b2a819bd26fbf0ab68fb6e8b7acd3278845
LouisDelbosc <louisdelbosc@gmail.com>
2015-06-30 17:42:56 +0200

    remove useless jsx-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.js$/, loaders: [ 'jsx-loader?harmony', 'babel'] } //to load jsx from .js file
+            { test: /\.js$/, loader: 'babel' } //to load jsx from .js file
         ]
     }
 };
